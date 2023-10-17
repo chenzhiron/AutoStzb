7f43464ea49b7f3ddb9a545c45d98ddc3d55b2ef
chenzhirong <2380723585@qq.com>
2023-10-17 17:40:01 +0800

    修正依赖包

--- ocr/main.py
+++ ocr/main.py
@@ -7,17 +7,6 @@ def ocr_default(paths):
     ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
     result = ocr.ocr(paths, cls=True)
     return result
-    # for idx in range(len(result)):
-    #     res = result[idx]
-    #     for line in res:
-    #         print(line)
-    # p2t = Pix2Text(analyzer_config=dict(model_name='mfd'))
-    # ocr = p2t.recognize(path, resized_shape=608, use_analyzer=False)
-    # print(ocr)
-    # if ocr:
-    #     return ocr[0]['text']
-    # else:
-    #     return []
 
 
 def ocr_txt_verify(path, auto_txt, area=(0, 0, 0, 0)):
