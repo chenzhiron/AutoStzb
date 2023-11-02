4d5bfb5fc892d5af4f7ab7c4924f85e7745c8e4e
chenzhirong <2380723585@qq.com>
2023-11-02 10:31:14 +0800

    更新bat启动文件、更新说明文档

--- start.bat
+++ start.bat
@@ -3,5 +3,12 @@
 
 chcp 65001
 
+.\toolkit\python.exe .\toolkit\get-pip.py
+
+.\toolkit\python.exe .\toolkit\Scripts\pip.exe install -r .\requirements.txt
+
 .\toolkit\python.exe .\start.py
+
 .\toolkit\python.exe .\main.py
+
+pause
