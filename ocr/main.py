35557d3d62d9c96bdf91234bd5053b7b0e794c23
chenzhirong <2380723585@qq.com>
2023-11-17 14:59:31 +0800

    添加本地模型

--- ocr/main.py
+++ ocr/main.py
@@ -1,6 +1,10 @@
 from paddleocr import PaddleOCR
 
-ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
+ocr = PaddleOCR(lang="ch",
+                det_model_dir="./ch_PP-OCRv4_det_infer",
+                rec_model_dir="./ch_PP-OCRv4_rec_infer",
+                cls_model_dir="./ch_ppocr_mobile_v2.0_cls_infer",
+                )
 
 
 def ocr_default(sources):
