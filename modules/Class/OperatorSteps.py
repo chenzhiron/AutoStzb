3a230c820d835cee2673294cd0e5fa08a65f2e21
chenzhirong <2380723585@qq.com>
2023-11-30 14:27:21 +0800

    查看战报兼容双模拟器格式

--- modules/Class/OperatorSteps.py
+++ modules/Class/OperatorSteps.py
@@ -51,6 +51,7 @@ class ClickOperatorSteps(OperatorSteps):
             return True
         return False
 
+
 class SwipeOperatorSteps(OperatorSteps):
     def __init__(self, area, txt, swipe_lists):
         super().__init__(area, txt)
