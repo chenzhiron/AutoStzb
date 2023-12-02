65440088110ca8797acd95f69e32df13f611114c
chenzhirong <2380723585@qq.com>
2023-12-02 11:39:53 +0800

    fix: 路径写错了 -_-||

--- start.bat
+++ start.bat
@@ -5,7 +5,7 @@ chcp 65001
 set SCRIPT_PATH=%~dp0
 set PATH=%PATH%;%SCRIPT_PATH%toolkit\Scripts
 
-%SCRIPT_PATH%toolkit\python.exe %SCRIPT_PATH%get_pip.py
+%SCRIPT_PATH%toolkit\python.exe %SCRIPT_PATH%toolkit\get_pip.py
 
 %SCRIPT_PATH%toolkit\python.exe %SCRIPT_PATH%toolkit\Scripts\pip.exe install --no-warn-script-location -r %SCRIPT_PATH%requirements.txt
 
