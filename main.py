ae254734942fd7eddd0f2c2e1855d98b5f9385be
chenzhirong <2380723585@qq.com>
2024-01-05 17:42:53 +0800

    关于adb控制实际并不需要开单独线程

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
 import os
 import sys
-import threading
 
 p = os.getcwd()
 sys.path.append(p)
 lib_p = os.path.join(p, 'toolkit', 'Lib', 'site-packages')
 sys.path.append(lib_p)
 
-from device.AutoMation import automation
-from device.operate import init
 from web.web import start_web
 from config.const import web_port
 
 if __name__ == '__main__':
     try:
-        # 模拟器截图方案
-        operate2 = threading.Thread(target=automation.automate)
-        operate2.setDaemon(True)
-        operate2.start()
-        # 模拟器点击方案
-        operate = threading.Thread(target=init)
-        operate.setDaemon(True)
-        operate.start()
-
         start_web(web_port)
 
     except Exception as e:
