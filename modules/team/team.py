bf3fe241b8accbebe2be860e2f3fbcc138fb3f03
chenzhirong <2380723585@qq.com>
2024-01-24 17:46:14 +0800

    去除原web配置

--- modules/team/team.py
+++
@@ -1,8 +0,0 @@
-from modules.team.Class.teamProp import TeamProp
-from config.config import userConfig
-
-teamLists = []
-
-for v in userConfig['TeamManager']:
-    teamLists.append(TeamProp(config=v))
-
