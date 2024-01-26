851ec7c040b203706c88b81a1a803940c7cdda84
chenzhirong <2380723585@qq.com>
2024-01-26 16:46:43 +0800

    更新证兵按钮识别

--- test/main.py
+++ test/main.py
@@ -6,12 +6,13 @@ from test.config.config import globalConfig
 from test.devices.device import Devices
 from ocr.main import ocrDefault
 device = Devices(globalConfig)
+area = (1263.0, 806.0, 1400.0, 835.0)
 if __name__ == '__main__':
     device.startDevices()
     time.sleep(3)
-    device.operateTap(410, 800)
-    for v in range(10):
-        img = device.getScreenshots()
-        res = ocrDefault(np.array(img))
-        print(res)
+    img = device.getScreenshots()
+    # res = ocrDefault(np.array(img))
+
+    res = ocrDefault(np.array(img.crop(area)))
+    print(res)
     device.stop()
