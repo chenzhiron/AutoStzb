4956eb4a948ed3adf6a634c9972d54340cc05388
chenzhiron <2380723585@qq.com>
2024-01-31 23:43:44 +0800

    阵容12345的不同编队位置

--- tests/main.py
+++ tests/main.py
@@ -6,18 +6,17 @@ from tests.devices.device import Devices
 from ocr.main import ocrDefault
 
 device = Devices(globalConfig)
-area = (444, 628, 462, 660)
+area = (1182, 628, 1210, 700)
 if __name__ == '__main__':
     try:
         device.startDevices()
-        device.operateTap(200, 340)
-        # img = device.getScreenshots()
-        # # res = ocrDefault(np.array(img))
-        # ress = img.crop(area)
-        # ress.save('1.png')
-        # res = ocrDefault(np.array(ress))
-        # print(res)
-        # device.closeDevice()
+        img = device.getScreenshots()
+        # res = ocrDefault(np.array(img))
+        ress = img.crop(area)
+        ress.save('1.png')
+        res = ocrDefault(np.array(ress))
+        print(res)
+        device.closeDevice()
     except:
         device.closeDevice()
 
