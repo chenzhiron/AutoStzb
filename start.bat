a9e6768e2564180f88c7e0cbfc64c84a8d23fd43
chenzhiron <2380723585@qq.com>
2024-12-13 00:36:06 +0800

    添加项目git更新

---
+++ start.bat
@@ -0,0 +1,14 @@
+@echo off
+
+chcp 65001
+
+:: 设置 Git 可执行文件路径和项目相关参数
+set GIT_PATH=.\toolkit\gitbash\cmd\git.exe
+set REPO_URL=https://github.com/chenzhiron/AutoStzb.git
+set LOCAL_DIR=%CD%
+
+"%GIT_PATH%" init
+"%GIT_PATH%" remote add origin "%REPO_URL%"
+"%GIT_PATH%" pull --depth=1 origin main
+
+pause
