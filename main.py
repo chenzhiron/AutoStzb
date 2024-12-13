03455de005f1ba40bc8991d0a2c4f98c164275ae
chenzhirong <2380723585@qq.com>
2024-12-13 13:56:01 +0800

    添加 bat 启动模块搜索

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# 动态添加上级目录
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
+
 if __name__ == "__main__":
     from modules.web.main import server
 
