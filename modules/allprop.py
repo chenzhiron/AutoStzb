836bfdea8793789a4929ef75da02d934671ea177
chenzhirong <2380723585@qq.com>
2024-12-16 18:44:12 +0800

    添加一部分属性

--- modules/allprop.py
+++ modules/allprop.py
@@ -7,11 +7,19 @@ def stPropManger():
         {
             simulator_address: "127.0.0.1:16384",
             besiegemain_state: False,
+            besiegemain_endtime: "2024/12/16 12:00:00",
             basiegedestory_state: False,
+            basiegedestory_endtime: "2024/12/16 12:00:00",
             exploit_state: False,
             enemymain_state: False,
+            enemymain_next_time: "2024/12/16 12:00:00",
+            enemymain_endtime: "2024/12/16 12:00:00",
             battledestory_state: False,
+            battledestory_next_time: "2024/12/16 12:00:00",
+            battledestory_endtime: "2024/12/16 12:00:00",
             myfight_state: False,
+            myfight_next_time: "2024/12/16 12:00:00",
+            myfight_endtime: "2024/12/16 12:00:00",
             ranking_state: False,
         }
     )
--- modules/static/propname.py
+++ modules/static/propname.py
@@ -2,16 +2,26 @@
 simulator_address = "simulator_address"
 
 # 打城主力
-besiegemain_state = "besiegemain_state_state"
+besiegemain_state = "besiegemain_state"
+besiegemain_endtime = "besiegemain_endtime"
 # 打城拆迁
 basiegedestory_state = "basiegedestory_state"
+basiegedestory_endtime = "basiegedestory_endtime"
 # 武勋
 exploit_state = "exploit_state"
 # 敌军主力
 enemymain_state = "enemymain_state"
+enemymain_next_time = "enemymain_next_time"
+enemymain_endtime = "enemymain_endtime"
+
 # 战场翻地/拆除
 battledestory_state = "battledestory_state"
+battledestory_next_time = "battledestory_next_time"
+battledestory_endtime = "battledestory_endtime"
+
 # 我方出战/防守
 myfight_state = "myfight_state"
+myfight_next_time = "myfight_next_time"
+myfight_endtime = "myfight_endtime"
 # 排行榜数据
 ranking_state = "ranking_state"
